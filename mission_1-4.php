d830c2a155c63933ef2261aed7af10e4d009a7a0
Taro-TechD-Project <44106682+Taro-TechD-Project@users.noreply.github.com>
2018-10-20 12:56:45 +0900

    Add files via upload

---
+++ mission_1-4.php
@@ -0,0 +1,17 @@
+﻿<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UFT-8">
+<title>フォームからデータを受け取る</title>
+</head>
+<body>
+<form method="post"action="mission_1-4.php">
+<p><input type="text"value="コメント"name="comment">
+<input type="submit"value="送信"></p>
+</form>
+</body>
+<?php
+$comment=$_POST['comment'];
+$date=date("Y年m月d日 H時i分s秒");
+echo"ご入力ありがとうございます。<br>".$date."に".$comment."を受け付けました</html>";
+?>
