db8c823c3c28e69d589bba764a2de90291a573f1
Taro-TechD-Project <44106682+Taro-TechD-Project@users.noreply.github.com>
2018-10-20 13:29:14 +0900

    Add files via upload

---
+++ mission_4-1.php
@@ -0,0 +1,14 @@
+﻿<?php
+$dsn='mysql:dbname=データベース名;host=localhost;charset=utf8';			/*<?php から ?>で挟まれるPHP領域に記載すること 毎回接続が必要*/
+$user='ユーザー名';								/*phpとMysqlの連携＋データベースへの接続＋データベース作成*/
+$password='パスワード';
+$pdo=new PDO($dsn,$user,$password);
+
+$sql='SHOW TABLES';
+$result=$pdo->query($sql);							/*テーブル一覧を表示するコマンドを使って作成が出来たか確認*/
+foreach($result as $row){
+echo $row[0];
+echo '<br>';
+}
+echo "<hr>";
+?>
