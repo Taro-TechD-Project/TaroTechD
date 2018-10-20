7cfd240b1b2e1758d4985f63b67c9b267c0ad0da
Taro-TechD-Project <44106682+Taro-TechD-Project@users.noreply.github.com>
2018-10-20 12:57:55 +0900

    Add files via upload

---
+++ mission_1-6.php
@@ -0,0 +1,21 @@
+﻿<DOCTYPE html>
+<html>
+<head>
+<meta=charset="utf-8">
+</head>
+<body>
+<form method="post" action="mission_1-6.php">
+<input type="text"value="コメント"name="comment">
+<input type="submit"value="送信">
+</form>
+</body>
+</html>
+<?php
+$comment=$_POST['comment'];
+if(!empty($_POST["comment"])){
+$filename="mission_1-6_Iwata.txt";
+$fp=fopen($filename,"a");
+fwrite($fp,$comment."\n");
+fclose($fp);
+}
+?>
